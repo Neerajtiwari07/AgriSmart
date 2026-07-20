ecd99ebc6e246fd8ccd6bd7bc525a6a5daa54034
Neeraj Tiwari <neerajtiwari202082@gmail.com>
2026-07-21 01:43:04 +0530

    Update CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://agri-smart-nu.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
